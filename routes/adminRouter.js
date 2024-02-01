b02d69a6621cb43d5230d87810ec119f78385e5f
D-General <kwabijoe274@gmail.com>
2024-02-01 22:05:24 +0000

    Added validation in the admins route

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const validationError = require("../utilities/validationError");
+const validationScheme = require("../utilities/validationScheme");
+const valid = [...validationScheme, validationError.validateRequestSchema];
 
 //Defining all routes for the various crud operations for the admin model
-router.post("/registerAdmin", adminController.signUpAdmin);
+router.post("/registerAdmin", valid, adminController.signUpAdmin);
 router.get("/getAllAdmins", adminController.getAllAdmins);
 router.patch("/updateAdmin/:id", adminController.updateAdminById);
 router.delete("/deleteAdmin/:id", adminController.deleteAdminById);
