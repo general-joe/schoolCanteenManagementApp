c67d11faafce8c7876bb0d51663a73781a614db4
lockwud <oklement3@gmail.com>
2024-02-08 01:20:42 -0800

    update

---
+++ src/routes/attendanceRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const attendance = require('../controllers/attendance');
+const {checkindexAvailablity} = require('../validations/middlewares/attendanceCheck')
+
+
+
+
+router.post('/markAttendance',  checkindexAvailablity,attendance.markAttendance)
+router.get('/getAttendance', attendance.getAllattendance)
+router.get('/:studentId', attendance.getAttendanceById)
+router.get('/date',  attendance.getAttendanceByDate)
+router.patch('/update/:id', attendance.updateAttendance)
+
+
+
+
+
+module.exports = router;
--- src/routes/studentRoute.js
+++ src/routes/studentRoute.js
@@ -1,12 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const studentController = require("../controllers/studentController");
+const Student = require('../controllers/student')
+const {studentAvailablity} = require('../validations/middlewares/studentAvailability')
 
-//Defining all routes for crud operations for student model
-router.post("/registerStudent", studentController.signUpStuent);
-router.get("/getAllStudents", studentController.getAllStudents);
-router.patch("/updateStudent/:id", studentController.updatStudentById);
-router.delete("/deleteStudent/:id", studentController.deleteStudentById);
-router.get("/:classId/students", studentController.getAllStudentByClassId);
 
+
+router.post('/register', studentAvailablity, Student.addStudent)// a post request to add a student
+router.get('/list', Student.getStudent) // a get request to retrieve a list of all student 
+router.get('/:id', Student.getStudentById) // a get request to retrieve a particular student
+router.delete('/delete', Student.deleteStudent) // a delete request to remove a student 
+router.patch('/:id', Student.updateStudent) // a patch request to update records of a student
+
+
+///exporting or router 
 module.exports = router;
